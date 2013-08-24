8cc835c9ca7892d787fb719e6b7a15db2216d7d1
Xu Wang <xyu.wang.0713@gmail.com>
2013-08-24 11:57:49 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IBeautySpaNyc::Application.config.secret_token = '4c29f3020a9a272792720e472249bd52fb46667d42ede54bc056a250e4f7ed5d9d9267c53f6a7567e93449f2dfb4a718dc092fc3d2441d825416f3a2810180a4'
